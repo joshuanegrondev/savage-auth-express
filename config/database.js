8f3662ee19bae3919377444c2119279ddedad431
Joshua Negron <69063454+joshuanegrondev@users.noreply.github.com>
2020-09-21 04:07:29 -0400

    first commit
    savage authorization app

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://joshua:15TvN2XnyR7w1UOI@cluster0.mizgi.mongodb.net/21-savage-express?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': '21-savage-express'
+};
